7cf6518ebe647cfd1ebb6c99c1d8d441ca5a77d9
Jeffrey davies <jeffpdavies@gmail.com>
2018-11-19 15:37:38 +1100

    Type: Modified card classes Body: added card classes to JS, changing card classes from static to dynamic deleted card classes from html.idex file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const icons = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o",
+"fa fa-paper-plane-o","fa fa-anchor","fa fa-anchor","fa fa-bolt","fa fa-bolt",
+"fa fa-cube","fa fa-cube","fa fa-anchor","fa fa-anchor","fa fa-leaf","fa fa-leaf",
+"fa fa-bicycle","fa fa-bicycle"];
 
 /*
  * Display the cards on the page
